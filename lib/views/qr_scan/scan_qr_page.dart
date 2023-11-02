e50bc190bae47d3a6e8ae6d3597a614ea9d1b89c
jessicahlm <jessicaahalimm@gmail.com>
2023-11-02 19:04:00 +0700

    coba utk ugd

--- lib/views/qr_scan/scan_qr_page.dart
+++ lib/views/qr_scan/scan_qr_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:modul_cam_qr_1046/constant/app_constant.dart';
-import 'package:modul_cam_qr_1046/views/qr_scan/scanner_error_widget.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class BarcodeScannerPageView extends StatefulWidget {
@@ -14,6 +11,19 @@ class BarcodeScannerPageView extends StatefulWidget {
 class _BarcodeScannerPageViewState extends State<BarcodeScannerPageView>
     with SingleTickerProviderStateMixin {
   BarcodeCapture? barcodeCapture;
+  bool isScanning = true;
+  String scannedText = "";
+  late AnimationController _lineController;
+
+  @override
+  void initState() {
+    super.initState();
+    _lineController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    _lineController.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,35 +47,46 @@ class _BarcodeScannerPageViewState extends State<BarcodeScannerPageView>
               startDelay: true,
               controller: MobileScannerController(torchEnabled: false),
               fit: BoxFit.contain,
-              errorBuilder: (context, error, child) {
-                return ScannerErrorWidget(error: error);
-              },
               onDetect: (capture) => setBarcodeCapture(capture),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                height: 100,
-                color: Colors.black.withOpacity(0.4),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            AnimatedBuilder(
+              animation: _lineController,
+              builder: (context, child) {
+                final boxSize = 200.0;
+                return Stack(
+                  alignment: Alignment.center,
                   children: [
-                    Center(
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width - 120,
-                        height: 50,
-                        child: FittedBox(
-                          child: GestureDetector(
-                            onTap: () => getUrlResult(),
-                            child: barcodeCaptureTextResult(context),
+                    Positioned(
+                      left: (MediaQuery.of(context).size.width - boxSize) / 2,
+                      top: 200.0,
+                      width: boxSize,
+                      height: boxSize,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(
+                            color: Colors
+                                .grey, // Ganti warna sesuai keinginan Anda
+                            width: 2.0,
                           ),
                         ),
                       ),
                     ),
+                    Positioned(
+                      left: 100,
+                      right: 100,
+                      top: 150.0 +
+                          (boxSize / 2 - 1) +
+                          (boxSize / 2) * _lineController.value,
+                      height: 2.0,
+                      child: Container(
+                        color:
+                            Colors.green, // Ganti warna sesuai keinginan Anda
+                      ),
+                    ),
                   ],
-                ),
-              ),
+                );
+              },
             ),
           ],
         );
@@ -73,35 +94,85 @@ class _BarcodeScannerPageViewState extends State<BarcodeScannerPageView>
     );
   }
 
-  Text barcodeCaptureTextResult(BuildContext context) {
-    return Text(
-      barcodeCapture?.barcodes.first.rawValue ??
-          LabelTextConstant.scanQrPlaceHolderlabel,
-      overflow: TextOverflow.fade,
-      style: Theme.of(context)
-          .textTheme
-          .headlineMedium!
-          .copyWith(color: Colors.white),
-    );
-  }
-
   void setBarcodeCapture(BarcodeCapture capture) {
     setState(() {
       barcodeCapture = capture;
+      if (barcodeCapture != null) {
+        scannedText = barcodeCapture!.barcodes.first.rawValue ?? "";
+        processScannedText();
+      } else {
+        scannedText = "";
+        // Tambahkan kembali perintah pemindaian setelah hasil tidak valid
+        isScanning = true;
+      }
     });
   }
 
-  void getUrlResult() {
-    final qrCode = barcodeCapture?.barcodes.first.rawValue;
-    if (qrCode != null) {
-      copyToClipboard(qrCode);
+  void processScannedText() {
+    if (isLink(scannedText)) {
+      // Ini adalah URL, tampilkan pesan sukses dan linknya
+      showSuccessMessage(scannedText);
+    } else {
+      // Bukan URL, tampilkan pesan invalid
+      showInvalidQRCodeMessage();
     }
   }
 
-  void copyToClipboard(String text) {
-    Clipboard.setData(ClipboardData(text: text));
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text(LabelTextConstant.txtonCopyingClipBoard)),
+  bool isLink(String text) {
+    return text.startsWith("http://Kelompok1PBP") ||
+        text.startsWith("https://Kelompok1PBP");
+  }
+
+  void showSuccessMessage(String link) {
+    // Navigasi kembali ke halaman sebelumnya dan tampilkan pesan sukses
+    Navigator.of(context).pop();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Sukses"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("QR Code sukses discan."),
+              SizedBox(height: 10),
+              Text("Link: $link"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Tutup"),
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  void showInvalidQRCodeMessage() {
+    // Tampilkan pesan invalid QR code
+    Navigator.of(context).pop();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("QR Code Invalid"),
+          content: Text("QR Code yang discan tidak valid."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Tutup"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _lineController.dispose();
+    super.dispose();
   }
 }
